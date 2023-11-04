04640ff7b94bd9261c19a95c06c2c683dcbbc671
Nicolas castañeda <niccastaeda1@misena.edu.co>
2023-11-03 21:33:37 -0500

    xammp

---
+++ htdocs/Legends/index.php
@@ -0,0 +1,49 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+function getDB(){
+    $dbhost = "localhost";
+    $dbname = "Legends_db";
+    $dbuser = "backend";
+    $dbpass = "12345";
+    $mysql_conn_string = "mysql:host=$dbhost;dbname=$dbname";
+    $dbConnection = new PDO($mysql_conn_string, $dbuser, $dbpass);
+    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbConnection;
+}
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->get('/consultaDatos', function ($request, $response, $args) {  //Defino los servicios
+	try{
+		$db =  getDB(); //Carga los datos
+		$sth = $db->prepare(
+			"SELECT id, usuario, contrasenia
+			from usuarios");//Consulta
+		$sth->execute(); //Ejecutamos la consulta
+		$test = $sth->fetchAll(PDO::FETCH_ASSOC);//Guardar los resultados de la consulta
+		//Verificar si se ha cargado algo
+		if($test){
+			$response->getBody()->write(json_encode($test)); //write Escribe la respuesta como texto, pero necesito un Json
+			$db = null;//Cerrar la conexion con la base de datos
+		}
+		else{
+			$response->getBody()->write('{"error":"error"}');
+		}
+	}catch(PDOException $e){
+			$response->getBody()->write('{"error":{"texto":'.$e->getMessage().'}}'); //En caso que se halla generado algún error
+		}
+    return $response
+    ->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import { ConexionService } from '../services/conexion.service';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +12,25 @@ export class HomePage {
 
   MyUser: string=''
   MyPass: string=''
+  activateRoute: any;
+
   constructor(private toastController: ToastController,
+              private conexion: ConexionService,
               private router: Router) {}
 
+  ngOnInit() {
+    // this.MyUser = this.activateRoute.snapshot.paramMap.get('nombre')
+    this.visualizaDatos()
+  }
+
+  visualizaDatos(){
+    this.conexion.consultaDatos().subscribe(
+      data => {
+        this.datos = data
+      }
+    )
+  }
+  
   enviar(){
     if(this.MyUser.length > 0){
       this.router.navigate(['../marcas',this.MyUser])

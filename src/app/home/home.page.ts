affa22c44e6c422d85154a22bfe707be027f6d42
Nicolas castañeda <niccastaeda1@misena.edu.co>
2023-11-03 20:04:19 -0500

    Agrega cambios home.page y conexion.service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  MyUser: string=''
+  MyPass: string=''
+  constructor(private toastController: ToastController,
+              private router: Router) {}
 
-}
+  enviar(){
+    if(this.MyUser.length > 0){
+      this.router.navigate(['../marcas',this.MyUser])
+    }else{
+      this.presentToast()
+    }
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Debe digitar sus credenciales',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+} 
